270812eabd4ad35a6370530047a0c02c0fd1b489
Ryan Sims <ryanjsims@email.arizona.edu>
2021-09-06 10:40:20 -0700

    Add progress to install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 LOGFILE="install.log"
-FLASK_APP=weatherportal
+export FLASK_APP=weatherportal
 CONFIG="/var/local/weathermap/instance/config.py"
 
+echo "Creating directory structure"
 cp init.d.sh /etc/init.d/weathermap > $LOGFILE 2>&1
 mkdir /var/local/weathermap >> $LOGFILE 2>&1
 mkdir /var/local/weathermap/cache >> $LOGFILE 2>&1
 mkdir /var/local/weathermap/cache/nowcast >> $LOGFILE 2>&1
 mkdir /var/local/weathermap/fonts >> $LOGFILE 2>&1
 
+echo "Copying files..."
 cp fonts/4x6.bdf /var/local/weathermap/fonts >> $LOGFILE 2>&1
 cp main.py /var/local/weathermap/weathermap.py >> $LOGFILE 2>&1
 cp -r weatherportal /var/local/weathermap/ >> $LOGFILE 2>&1
 
+echo "Initializing instance files"
 cd /var/local/weathermap
 flask init-db
 echo "import os" > $CONFIG
@@ -36,11 +39,13 @@ echo "    'pause': False" >> $CONFIG
 echo "}" >> $CONFIG
 cd -
 
+echo "Setting permissions"
 chgrp -R daemon /var/local/weathermap >> $LOGFILE 2>&1
 chmod -R 775 /var/local/weathermap >> $LOGFILE 2>&1
 chmod +x /etc/init.d/weathermap >> $LOGFILE 2>&1
 chmod 660 $CONFIG
 
+echo "Updating init.d"
 update-rc.d weathermap defaults >> $LOGFILE 2>&1
 echo -n "Done." >> $LOGFILE 2>&1
 
