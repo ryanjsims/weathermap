be354b3d9a1250a5f8396b32fab033826cf928d6
Ryan Sims <ryanjsims@email.arizona.edu>
2021-09-12 18:27:19 -0700

    Add holidays to database and related images

--- weatherportal/schema.sql
+++ weatherportal/schema.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS credentials;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS birthdays;
 DROP TABLE IF EXISTS schedules;
+DROP TABLE IF EXISTS config;
+DROP TABLE IF EXISTS holidays;
 
 CREATE TABLE credentials (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -48,7 +50,62 @@ CREATE TABLE config (
   img_size INTEGER NOT NULL,
   refresh_delay INTEGER NOT NULL,
   pause BOOLEAN NOT NULL,
+  realtime BOOLEAN NOT NULL
 );
 
-insert into config (size, lat, lon, z, color, options, dimensions, img_size, refresh_delay, pause) 
-            values (256, 33.317027, -111.875500, 9, 4, '0_0', 200000, 64, 5, false);
+CREATE TABLE palette (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  color1 INTEGER,
+  color2 INTEGER,
+  color3 INTEGER,
+  color4 INTEGER
+);
+
+CREATE TABLE holidays (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  message TEXT,
+  path TEXT,
+  palette_id INTEGER,
+  FOREIGN KEY (palette_id) REFERENCES palette (id)
+);
+
+insert into config (size, lat, lon, z, color, options, dimensions, img_size, refresh_delay, pause, realtime) 
+            values (256, 33.317027, -111.875500, 9, 4, '0_0', 200000, 64, 5, false, false);
+
+--Christmas
+insert into palette (color1, color2) values (0xE40A2D, 0x1FD537);
+--Halloween
+insert into palette (color1, color2) values (0xF4831B, 0x902EBB);
+--Chinese New Year
+insert into palette (color1, color2) values (0xCC232A, 0xCC9902);
+--Valentines 
+insert into palette (color1, color2) values (0xE07498, 0xC45E94);
+--St Patrick's Day 
+insert into palette (color1, color2) values (0x148E48, 0xF77E33);
+--Juneteenth
+insert into palette (color1, color2) values (0xE31C23, 0x00843E);
+--4th of July
+insert into palette (color1, color2, color3) values (0x3C3B6E, 0xFFFFFF, 0xB22234);
+--Mexican Independance day
+insert into palette (color1, color2, color3) values (0x3C3B6E, 0xFFFFFF, 0xB22234);
+--Thanksgiving
+insert into palette (color1, color2) values (0xBE5634, 0xDEBF7E);
+
+insert into holidays (name, message, path, palette_id) values ('New Year''s Day', '...in with the new!', './static/images/hourglass_full.png', NULL);
+insert into holidays (name, message, path, palette_id) values ('Dr. Martin Luther King Jr./Civil Rights Day', 'MLK Jr. Day', NULL, NULL);
+insert into holidays (name, message, path, palette_id) values ('Chinese New Year', 'Lunar New Year', NULL, 3);
+insert into holidays (name, message, path, palette_id) values ('Valentine''s Day', 'Will you be my Valentine?', './static/images/heart.png', 4);
+insert into holidays (name, message, path, palette_id) values ('St. Patrick''s Day', 'It looks pretty Irish out there!', './static/images/clover.png', 5);
+insert into holidays (name, message, path, palette_id) values ('Easter Sunday', 'Happy Easter!', './static/images/cross.png', NULL);
+insert into holidays (name, message, path, palette_id) values ('Mother''s Day', 'Yay for Mom!', './static/images/heart.png', NULL);
+insert into holidays (name, message, path, palette_id) values ('Juneteenth National Independence Day', 'Juneteenth', './static/images/juneteenth.png', 6);
+insert into holidays (name, message, path, palette_id) values ('Independence Day', 'America the Beautiful', NULL, 7);
+insert into holidays (name, message, path, palette_id) values ('Día de la Independencia', '¡Viva Mexico!', NULL, 8);
+insert into holidays (name, message, path, palette_id) values ('Halloween', 'Trick or Treat?', './static/images/pumpkin.png', 2);
+insert into holidays (name, message, path, palette_id) values ('Día de los Muertos', 'Dia de los Muertos', './static/images/skull.png', NULL);
+insert into holidays (name, message, path, palette_id) values ('Veterans Day', 'Celebrate our Veterans', NULL, NULL);
+insert into holidays (name, message, path, palette_id) values ('Thanksgiving', 'What are you thankful for?', NULL, 9);
+insert into holidays (name, message, path, palette_id) values ('Christmas Eve', NULL, './static/images/christmas_tree.png', 1);
+insert into holidays (name, message, path, palette_id) values ('Christmas Day', 'Ho ho ho!', './static/images/christmas.png', 1);
+insert into holidays (name, message, path, palette_id) values ('New Year''s Eve', 'Out with the old...', './static/images/hourglass_empty.png', NULL);
